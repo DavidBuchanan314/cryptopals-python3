ce8f8dc13f172375fa3752583626fc15f21525f4
David Buchanan <d@vidbuchanan.co.uk>
2018-07-25 22:02:20 +0100

    Improve challenge 3

--- 01-basics/03-singlebyte-xor.py
+++ 01-basics/03-singlebyte-xor.py
@@ -3,9 +3,9 @@ from utils import *
 
 if __name__ == "__main__":
 	data = dehex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-	keyspace = list(range(0x100))
+	keyspace = list()
 	
-	_, best_key  = min((englishness(xor(data, [key])), key) for key in keyspace)
+	best_key  = min(range(0x100), key=lambda k:englishness(xor(data, [k])))
 	
 	message = xor(data, [best_key]).decode()
 	assert(message == "Cooking MC's like a pound of bacon")
